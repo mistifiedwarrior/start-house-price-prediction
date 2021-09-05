7863b94581d39251588e6d8742868632b37b182d
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-09-05 21:26:27 +0530

    [#1] | [Shivi/Aadii] | update init command

--- init.sh
+++ init.sh
@@ -2,16 +2,17 @@
 
 #say "Task started"
 echo "Cloning UI..."
-git clone https://github.com/mistifiedwarrior/housePricePredictionUI.git ui
+git clone https://github.com/mistifiedwarrior/housePricePredictionUI.git ui &> /dev/null
 cd ui || exit
 echo "Installing dependencies..."
-npm install 2> /dev/null
+npm install &> /dev/null
 echo "Start building..."
-npm run build 2> /dev/null
-mv ui/build/* .
+npm run build &> /dev/null
+mv build/* .
 
+cd ..
 echo "cloning ML model"
-git clone https://github.com/mistifiedwarrior/house_price_prediction.git ml
+git clone https://github.com/mistifiedwarrior/house_price_prediction.git ml  &> /dev/null
 echo "Task Completed"
 #say "Task Completed"
 
